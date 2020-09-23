a86c82a5b35f7bd53ea7aed180be4a78d2cf4046
Jonah <jonah.blaydes-greenberg@menloschool.org>
2020-09-23 13:57:40 -0700

    Class 2

--- crypto.py
+++ crypto.py
@@ -4,13 +4,13 @@
 def encrypt_caesar(plaintext, offset):
     encrypt_dummy = []
 
-    for i in ciphertext:
-        if ord(ciphertext[i]) < ord('A') or ord(ciphertext[i]) > ord('Z'):
+
+
+    for i in plaintext:
+        if ord(i) < ord('A') or ord(i) > ord('Z'):
             return "Incorrect Input"
-        encrypt_dummy.append(ord(ciphertext(i)))
-        encrypt_dummy[i] + offset
-        encrypt_dummy[i] = chr(encrypt_dummy[i]) 
-    
+        encrypt_dummy.append(chr(ord('A') + ((ord(i) - ord('A') + offset) % 26)))
+
     return ''.join(encrypt_dummy)
 
 # Arguments: string, integer
@@ -18,25 +18,33 @@ def encrypt_caesar(plaintext, offset):
 def decrypt_caesar(ciphertext, offset):
     encrypt_dummy = []
 
-    if ord(ciphertext[i]) < ord('A') or ord(ciphertext[i]) > ord('Z'):
-            return "Incorrect Input"
-
     for i in ciphertext:
-        encrypt_dummy.append(ord(ciphertext(i)))
-        encrypt_dummy[i] = encrypt_dummy[i] - offset
-        encrypt_dummy[i] = chr(encrypt_dummy[i])
+        if ord(i) < ord('A') or ord(i) > ord('Z'):
+            return "Incorrect Input"
+        encrypt_dummy.append(chr(ord('A') + ((ord(i) - ord('A') - offset) % 26)))
+
     return ''.join(encrypt_dummy)
 
 # Vigenere Cipher
 # Arguments: string, string
 # Returns: string
 def encrypt_vigenere(plaintext, keyword):
-    pass
+    for i in plaintext:
+
+        if ord(i) < ord('A') or ord(i) > ord('Z'):
+            return "Incorrect Input"
+        offset = ord(keyword[i % len(keyword)]) - ord('A')
+        encrypt_dummy.append(chr(ord('A') + ((ord(i) - ord('A') + offset) % 26)))
 
 # Arguments: string, string
 # Returns: string
 def decrypt_vigenere(ciphertext, keyword):
-    pass
+    
+    for i in ciphertext:
+        if ord(i) < ord('A') or ord(i) > ord('Z'):
+            return "Incorrect Input"
+        offset = keyword[i % len(keyword)] - ord('A')
+        encrypt_dummy.append(chr(ord('A') + ((ord(i) - ord('A') - offset) % 26)))
 
 # Merkle-Hellman Knapsack Cryptosystem
 # Arguments: integer
@@ -60,7 +68,7 @@ def decrypt_mhkc(ciphertext, private_key):
     pass
 
 def main():
-    print encrypt_caesar("MARVIN", 1)
+    print(encrypt_vigenere('ZACH' , 'AAAA'))
     
 
 if __name__ == "__main__":
